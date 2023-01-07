8c55545851bc19ce5d511917e5b188d8b3fcffda
gabmotamelo <gabrielmota6@hotmail.com>
2023-01-06 21:50:38 -0300

    Criando estrutura projeto

---
+++ src/test/java/gerenciador-app/GerenciadorPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package attornatus.gerenciadorpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
