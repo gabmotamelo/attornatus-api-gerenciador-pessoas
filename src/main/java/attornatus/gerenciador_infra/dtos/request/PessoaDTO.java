8830883b51fe1ac166a5a2067d1d78c6930e5940
gabmotamelo <gabrielmota6@hotmail.com>
2023-01-06 23:54:44 -0300

    Ajustando Swagger

---
+++ src/main/java/attornatus/gerenciador_infra/dtos/request/PessoaDTO.java
@@ -0,0 +1,34 @@
+package attornatus.gerenciador_infra.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PessoaDTO {
+
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String nomeCompleto;
+
+    @NotNull
+    private LocalDate dataDeNascimento;
+
+    @Valid
+    @NotBlank
+    private List<EnderecoDTO> enderecos;
+
+}
